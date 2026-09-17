1c6bafe7a3d39a4adcf85cc77c4f4292d07876b6
debashisadhikari <debashisadhikari523@gmail.com>
2026-09-17 18:03:01 +0530

    setup server connect mongo test api

--- E-commerce-backend/server.js
+++ E-commerce-backend/server.js
@@ -1,3 +1,33 @@
-const express=required("express");
-const app=express();
-const PORT=5001;
+const express = require("express");
+const mongoose = require("mongoose");
+const cors=require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is running ok");
+});
+app.get("/api/products",(req,res,next)=>{
+  res.json({
+    message:"hello this is from backend",
+  })
+});
+
+const PORT = 5001;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error while connecting DB:", err);
+  });
--- E-commerce-frontend/src/App.jsx
+++ E-commerce-frontend/src/App.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+
 const App=()=>{
+  const [message,setMessage]=useState();
+  useEffect(() => {
+    fetch("http://localhost:5001/api/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  }, []);
   return <>
-  <h1>this is first page</h1>
+  <h1>{message}</h1>
   </>
 }
 export default App;
